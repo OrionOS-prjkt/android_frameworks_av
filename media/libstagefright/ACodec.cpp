4eba9088333d67fbdab22fcc0ebd38760ed7660a
Hirokixd <86230878+Wrdn28@users.noreply.github.com>
2024-07-14 18:14:40 +0000

    Revert "Add AC4Tbl params for dolby AC4 decoder [1/2]"
    because already added
    
    This reverts commit cf96627deab2595257bbdfb9a97c28c011c712f9.

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -64,7 +64,6 @@
 #include "include/SecureBuffer.h"
 #include "include/SharedMemoryBuffer.h"
 #include <media/stagefright/omx/OMXUtils.h>
-#include "TableXInit.h"
 
 #include <server_configurable_flags/get_flags.h>
 #include "TableXInit.h"
